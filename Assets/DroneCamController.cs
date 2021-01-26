e111ffc807ea72f6dd59e8698a61c1642f6fef4f
CensedPie <alex.savu@hotmail.com>
2021-01-27 00:05:19 +0200

    Commited all files for project

---
+++ Assets/DroneCamController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneCamController : MonoBehaviour
+{
+    public float camSensitivity = 100f;
+    float xRotation = 0f;
+    float yRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axisX = Input.GetAxis("DroneCamHorizontal") * camSensitivity * Time.deltaTime;
+        float axisY = Input.GetAxis("DroneCamVertical") * camSensitivity * Time.deltaTime;
+
+        xRotation -= axisY;
+        xRotation = Mathf.Clamp(xRotation, -20f, 90f);
+        yRotation += axisX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
---
+++ Assets/DroneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float side = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.position = new Vector3(transform.position.x + side, transform.position.y, transform.position.z + forward);
+    }
+}
